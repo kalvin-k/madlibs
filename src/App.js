bf4311e7aab01d50d4a819dae05f415e94d674e2
learn <learn@learn08.local>
2019-10-15 15:42:08 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -31,9 +31,24 @@ class App extends Component {
 
     handleSubmit = (e) => {
         const { form, userForm } = this.state
-        let newForm = form.keys
         this.setState({form: userForm})
 }
+
+    clearForm = () => {
+        const { userForm } = this.state
+
+        const userForm2 = {
+            noun: '',
+            adj: '',
+            adj2: '',
+            adverb: '',
+            verb: '',
+        }
+
+        this.setState({userForm: userForm2})
+
+}
+
 
     render() {
 
@@ -42,41 +57,44 @@ class App extends Component {
         return (
           <div>
           <center>
-            <h3> Noun: </h3>
-            <input
-            name = 'noun'
-            value = {noun}
-            onChange = {this.handleChange}
-            />
-            <br />
-            <h3> Adjective: </h3>
-            <input
-            name = 'adj'
-            value = {adj}
-            onChange = {this.handleChange}
-            />
-            <br />
-            <h3> Adjective 2: </h3>
-            <input
-            name = 'adj2'
-            value = {adj2}
-            onChange = {this.handleChange}
-            />
-            <br/>
-            <h3> Adverb:  </h3>
-            <input
-            name = 'adverb'
-            value = {adverb}
-            onChange = {this.handleChange}
-            />
-            <br />
-            <h3> Verb </h3>
-            <input
-            name = 'verb'
-            value = {verb}
-            onChange = {this.handleChange}
-            />
-            <br/>
+          <h1> MabLibs!! </h1> 
+            <div id = 'testing'>
+                <h3> Noun: </h3>
+                <input
+                name = 'noun'
+                value = {noun}
+                onChange = {this.handleChange}
+                />
+                <br />
+                <h3> Adjective: </h3>
+                <input
+                name = 'adj'
+                value = {adj}
+                onChange = {this.handleChange}
+                />
+                <br />
+                <h3> Adjective 2: </h3>
+                <input
+                name = 'adj2'
+                value = {adj2}
+                onChange = {this.handleChange}
+                />
+                <br/>
+                <h3> Adverb:  </h3>
+                <input
+                name = 'adverb'
+                value = {adverb}
+                onChange = {this.handleChange}
+                />
+                <br />
+                <h3> Verb </h3>
+                <input
+                name = 'verb'
+                value = {verb}
+                onChange = {this.handleChange}
+                />
+                <br/>
+            </div>
             <div>
                 <p>
                 Be kind to your {this.state.form.noun}-footed cats, for a duck may be somebody's {this.state.form.noun}. Coding is {this.state.form.adj} but somtimes its also {this.state.form.adj2}.
@@ -85,6 +103,9 @@ class App extends Component {
             </div>
             <button onClick = {this.handleSubmit}>
                 Submit
+            </button>
+            <button onClick = {this.clearForm}>
+                Clear
             </button>
             </center>
           </div>
