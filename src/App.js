431b0f0e5e711a7916d1f84641d79bbd77130dc7
learn <learn@learn08.local>
2019-10-15 15:00:00 -0700

    madlibs-game

---
+++ src/App.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react'
+import './App.css';
+
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            form: {
+                noun: '',
+                adj: '',
+                adj2: '',
+                adverb: '',
+                verb: '',
+            },
+            userForm: {
+                    noun: '',
+                    adj: '',
+                    adj2: '',
+                    adverb: '',
+                    verb: '',
+            }
+    }
+}
+
+    handleChange = (e) => {
+        const { userForm } = this.state
+        userForm[e.target.name] = e.target.value
+        this.setState({userForm: userForm})
+        console.log(userForm);
+    }
+
+    handleSubmit = (e) => {
+        const { form, userForm } = this.state
+        let newForm = form.keys
+        this.setState({form: userForm})
+}
+
+    render() {
+
+        let { noun, adj, adverb, verb, adj2 } = this.state.userForm
+
+        return (
+          <div>
+          <center>
+            <h3> Noun: </h3>
+            <input
+            name = 'noun'
+            value = {noun}
+            onChange = {this.handleChange}
+            />
+            <br />
+            <h3> Adjective: </h3>
+            <input
+            name = 'adj'
+            value = {adj}
+            onChange = {this.handleChange}
+            />
+            <br />
+            <h3> Adjective 2: </h3>
+            <input
+            name = 'adj2'
+            value = {adj2}
+            onChange = {this.handleChange}
+            />
+            <br/>
+            <h3> Adverb:  </h3>
+            <input
+            name = 'adverb'
+            value = {adverb}
+            onChange = {this.handleChange}
+            />
+            <br />
+            <h3> Verb </h3>
+            <input
+            name = 'verb'
+            value = {verb}
+            onChange = {this.handleChange}
+            />
+            <br/>
+            <div>
+                <p>
+                Be kind to your {this.state.form.noun}-footed cats, for a duck may be somebody's {this.state.form.noun}. Coding is {this.state.form.adj} but somtimes its also {this.state.form.adj2}.
+                When I {this.state.form.verb} I get tired. so {this.state.form.adverb} running...
+                </p>
+            </div>
+            <button onClick = {this.handleSubmit}>
+                Submit
+            </button>
+            </center>
+          </div>
+        );
+      }
+    }
+
+
+export default App;
